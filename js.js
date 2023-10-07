7b69354cba8cb41e039a428013abf5443559555a
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2023-10-07 18:43:59 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,27 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // typing 1
-      const typed = new Typed(".typing-1", {
+      const typed1 = new Typed(".typing-1", {
           strings: [
-              "<span style='color:white'>Hi, my name is <strong style='color:white'>Nehal Khan</strong></span><br><span style='color:white'>I'm a</span>",
+              "<span style='color:white'>Hi, my name is <strong style='color:white'>Nehal Khan</strong></span>",
             ],
           typeSpeed: 70,
           showCursor: false
       });
-  
+
   // typing 2
       setTimeout(() => {
       const typed2 = new Typed(".typing-2", {
+          strings: [
+              "<span style='color:white'>I'm</span>",
+            ],
+          typeSpeed: 70,
+          showCursor: false
+      });
+    }, 3000);
+      
+  // typing 3
+      setTimeout(() => {
+      const typed3 = new Typed(".typing-3", {
         strings: [
-          "<span style='color:black'> Programmer</span>",
-          "<span style='color:black'> Artist</span>",
-          "<span style='color:black'> leader</span>",
+          "<span style='color:black'>a Program Dev </span>",
+          "<span style='color:black'>a Competitive Solver</span>",
+          "<span style='color:black'>a Passionate Artist</span>",
+          "<span style='color:black'>an Inspiring Leader</span>",
         ],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
+        showCursor: false
       });
     }, 4000);
 
 });
   
+
