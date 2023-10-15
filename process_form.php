a8b9b12d6e89a4d44d64aabefe4b5eda2a02514b
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2023-10-15 18:54:47 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -6,10 +6,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
 
     $to = "nehalmicro29@gmail.com";
-    $subject = "Contact Form Submission - $subject";
-    $message = "Name: $name\nEmail: $email\n\n$message";
+    $email_subject = "Contact Form Submission - $subject"; // Changed this variable to avoid conflict
+    $email_message = "Name: $name\nEmail: $email\n\n$message"; // Changed this variable to avoid conflict
 
-    mail($to, $subject, $message);
-    header("Location: thank_you.html");
+    // You should set additional headers for your email, like 'From', 'Reply-To', and 'Content-Type'.
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Send the email
+    mail($to, $email_subject, $email_message, $headers);
+
+    // Redirect to a thank you page
+    exit; // Exit to prevent further execution
 }
 ?>
